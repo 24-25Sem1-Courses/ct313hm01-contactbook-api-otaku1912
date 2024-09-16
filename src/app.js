d8265a0cf0efd1fb813066bfb8f7c7ce096e8b75
otaku1912 <bortz.diamond39@gmail.com>
2024-09-17 01:58:13 +0700

    Standardize JSON responses

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
 const express = require('express'); 
 const cors = require('cors');
 
+const JSend = require('./jsend');
 const contactsRouter = require('./routes/contacts.router');
+
 const app = express();
 
 app.use(cors()); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => { return res.json({
-message: 'ok'
-});
+app.get('/', (req, res) => {
+return res.json(JSend.success());
 });
 
+
 contactsRouter.setup(app); 
+
 module.exports = app;
