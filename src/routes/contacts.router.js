5c48c991414dba2f675a02d60ef9296d7941d164
otaku1912 <bortz.diamond39@gmail.com>
2024-09-17 17:31:27 +0700

    Fix some bugs

--- src/routes/contacts.router.js
+++ src/routes/contacts.router.js
@@ -9,41 +9,41 @@ app.use('/api/v1/contacts', router);
 /**
  * @swagger
  * /api/v1/contacts:
- * get:
- *  summary: Get contacts by filter
- *  description: Get contacts by filter
- *  parameters:
- *      - in: query
- *        name: favorite
- *        schema:
- *          type: boolean
- *        description: Filter by favorite status
- *      - in: query
- *        name: name
- *        schema:
- *          type: string
- *        description: Filter by contact name
- *  tags: 
- *      - contacts
- *  responses:
- *      200:
- *        description: A list of contacts
- *        content:
- *          application/json:
- *              schema:
- *                  type: object
- *                  properties: 
- *                      status:
- *                          type: string
- *                          description: The response status
- *                          enum: [success]
- *                      data:
+ *  get:
+ *      summary: Get contacts by filter
+ *      description: Get contacts by filter
+ *      parameters:
+ *        - in: query
+ *          name: favorite
+ *          schema:
+ *              type: boolean
+ *          description: Filter by favorite status
+ *        - in: query
+ *          name: name
+ *          schema:
+ *              type: string
+ *          description: Filter by contact name
+ *      tags: 
+ *        - contacts
+ *      responses:
+ *          200:
+ *              description: A list of contacts
+ *              content:
+ *                  application/json:
+ *                      schema:
  *                          type: object
- *                          properties:
- *                              contacts:
- *                                  type: array
- *                                  items:
- *                                      $ref:: '#/components/schemas/Contact'
+ *                          properties: 
+ *                              status:
+ *                                  type: string
+ *                                  description: The response status
+ *                                  enum: [success]
+ *                              data:
+ *                                  type: object
+ *                                  properties:
+ *                                      contacts:
+ *                                          type: array
+ *                                          items:
+ *                                              $ref: '#/components/schemas/Contact'
  */
 
 router.get('/', contactsController.getContactsByFilter);
@@ -51,35 +51,35 @@ router.get('/', contactsController.getContactsByFilter);
 /**
  * @swagger
  * /api/v1/contacts:
- * post:
- *  summary: Create a new contact
- *  description: Create a new contact
- *  requestBody:
- *      required: true
- *      content: 
- *      multipart/form-data:
- *          schema:
- *              $ref: '#/components/schemas/Contact'
- * 
- *  tags:
- *      - contacts
- *  responses:
- *      201:
- *          description: A new contact
+ *  post:
+ *      summary: Create a new contact
+ *      description: Create a new contact
+ *      requestBody:
+ *          required: true
  *          content: 
- *              application/json:
+ *              multipart/form-data:
  *                  schema:
- *                      type: object
- *                      properties:
- *                          status:
- *                              type: string
- *                              description: The response status
- *                              enum: [success]
- *                          data:
- *                              type: object
- *                              properties:
- *                                  contact:
- *                                      $ref: '#/components/schemas/Contact'
+ *                      $ref: '#/components/schemas/Contact'
+ * 
+ *      tags:
+ *          - contacts
+ *      responses:
+ *          201:
+ *              description: A new contact
+ *              content: 
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              status:
+ *                                  type: string
+ *                                  description: The response status
+ *                                  enum: [success]
+ *                              data:
+ *                                  type: object
+ *                                  properties:
+ *                                      contact:
+ *                                          $ref: '#/components/schemas/Contact'
  */
 router.post('/', contactsController.createContact);
 
